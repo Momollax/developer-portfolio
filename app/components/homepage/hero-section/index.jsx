164274365e6aaaf9060fabdf18c1c53dafdfdee9
noé m <noe.moyen@epitech.eu>
2024-03-18 22:58:53 +0100

    ok

--- app/components/homepage/hero-section/index.jsx
+++ app/components/homepage/hero-section/index.jsx
@@ -10,6 +10,7 @@ import { RiContactsFill } from "react-icons/ri";
 import { SiLeetcode } from "react-icons/si";
 
 function HeroSection() {
+  
   return (
     <section className="relative flex flex-col items-center justify-between py-4 lg:py-12">
       <Image
